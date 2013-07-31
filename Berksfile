5b528ea89065fba05793c1676cf4cac5f54ada89
Julian C. Dunn <jdunn@opscode.com>
2013-07-31 18:07:25 -0400

    Initial commit of petclinic cookbook -- unfinished

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+metadata
+
+cookbook 'wlp', path: '../wlp'
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'petclinic'
+maintainer       'Julian C. Dunn'
+maintainer_email 'jdunn@opscode.com'
+license          'Apache 2.0'
+description      'Installs the Spring MVC Pet Clinic sample app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'oracle-xe'
+depends          'wlp'
